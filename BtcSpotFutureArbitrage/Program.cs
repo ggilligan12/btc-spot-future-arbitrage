9ced1b2a005063b5a8bdb9a521ea8c1d3d185ab0
george <ggilligan12@gmail.com>
2023-05-14 23:58:58 +0100

    Sending a subscribe request

--- BtcSpotFutureArbitrage/Program.cs
+++ BtcSpotFutureArbitrage/Program.cs
@@ -1,14 +1,27 @@
 ﻿using BtcSpotFutureArbitrage;
 
 string binanceURL = "wss://stream.binance.com:9443/ws/btcusdt@depth5";
-
-
-Console.WriteLine("Attempting to use our Web Socket class!");
-
 WSClientSingleton client = new WSClientSingleton();
 
+Console.WriteLine("Making WS connection...");
 await client.ConnectAsync(binanceURL);
+Console.WriteLine("Made connection!");
+
+Console.WriteLine("Subscribing to stream...");
+var garbage = @"{
+  ""method"": ""SUBSCRIBE"",
+  ""params"": [
+    ""btcusdt@aggTrade"",
+    ""btcusdt@depth""
+  ],
+  ""id"": 1
+}";
+await client.SendMessageAsync(garbage);
 
 
 
+System.Threading.Thread.Sleep(1000);
+
+Console.WriteLine("Closing connection...");
 await client.DisconnectAsync();
+Console.WriteLine("Connection closed. Done.");
